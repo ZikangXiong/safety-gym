a10e3ad8c445c97f27a882efb5966ad85202dcf4
ZikangXiong <zikangxiong@icloud.com>
2021-05-27 18:19:49 -0400

    before pre-compute

--- safety_gym/envs/engine.py
+++ safety_gym/envs/engine.py
@@ -1576,6 +1576,7 @@ class Engine(gym.Env, gym.utils.EzPickle):
 
     def select_training_phase(self, *args, **kwargs):
         # call this after every epoch to select training phase
+        # 5.0
         if np.mean(self.cum_subgoal_rewards[:-1]) > 5.0:
             return 1
         else:
